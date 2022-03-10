1fdafb82c99b63bec02e66ea56e5ee48e198501a
carlosaw <carlosfreevg@gmail.com>
2022-03-10 16:37:05 -0400

    Criando os Models

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    public $timestamps = false;
+
+    protected $hidden = [
+        'password'
+    ];
+
+    // Função adicionada p/ JWT
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    // Função adicionada p/ JWT
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ database/migrations/2022_03_10_190433_createalltables.php
@@ -0,0 +1,142 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Createalltables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   // Cria tabela de USUÁRIOS
+        Schema::create('users', function(Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('password');
+        });
+        // Cria a tabela da UNIDADE (apto)
+        Schema::create('units', function(Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('id_owner');
+        });
+        // Cria tabela de DONO APTO.
+        Schema::create('unitpeoples', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('name');
+            $table->date('birthdate');
+        });
+        // Cria tabela de VEÍCULOS
+        Schema::create('unitvehicles', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('title');
+            $table->string('color');
+            $table->string('plate');
+        });
+        // Cria tabela de PETS
+        Schema::create('unitpets', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('name');
+            $table->string('race');
+        });
+        // Cria tabela o MURAL
+        Schema::create('walls', function(Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->datetime('datecreated');
+        });
+        // Cria tabela o like do MURAL
+        Schema::create('walllikes', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_wall');
+            $table->integer('id_user');
+        });
+        // Cria tabela DOCUMENTOS
+        Schema::create('docs', function(Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('fileurl');
+        });
+        // Cria tabela BOLETOS
+        Schema::create('billets', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('title');
+            $table->string('fileurl');
+        });
+        // Cria tabela LIVRO DE OCORRÊNCIAS
+        Schema::create('warnings', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('title');
+            $table->string('status')->default('IN_REVIEW');// ou RESOLVED
+            $table->date('datecreated');
+            $table->text('photos');// foto1.jpg, foto2.jpg, foto3.jpg
+        });
+        // Cria tabela ACHADOS E PERDIDOS
+        Schema::create('foundandlost', function(Blueprint $table) {
+            $table->id();
+            $table->string('status')->default('LOST');// ou RECOVERED
+            $table->string('photo');
+            $table->string('description');
+            $table->string('where');
+            $table->date('datecreated');
+        });
+        // Cria tabela ÁREAS
+        Schema::create('areas', function(Blueprint $table) {
+            $table->id();
+            $table->integer('allowed')->default(1);// ou '0'
+            $table->string('title');
+            $table->string('cover');
+            $table->string('days');// 0,1,2,3,4,5,6
+            $table->time('start_time');
+            $table->time('end_time');
+        });
+        // Cria tabela ÁREAS INDISPONÍVEIS
+        Schema::create('areadisableddays', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_area');
+            $table->date('day');
+        });
+        // Cria tabela RESERVAS
+        Schema::create('reservations', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_unit');
+            $table->integer('id_area');
+            $table->datetime('reservation_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('units');
+        Schema::dropIfExists('unitpeoples');
+        Schema::dropIfExists('unitvehicles');
+        Schema::dropIfExists('unitpets');
+        Schema::dropIfExists('walls');
+        Schema::dropIfExists('walllikes');
+        Schema::dropIfExists('docs');
+        Schema::dropIfExists('billets');
+        Schema::dropIfExists('warnings');
+        Schema::dropIfExists('foundandlost');
+        Schema::dropIfExists('areas');
+        Schema::dropIfExists('areadisableddays');
+        Schema::dropIfExists('reservations');
+    }
+}
